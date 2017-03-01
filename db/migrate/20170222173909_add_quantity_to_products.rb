694319b6c04df889b5762ceae3476c255293e67f
Tmaikowski <Troy.maikowski@gmail.com>
2017-03-01 15:27:00 -0600

    Fixing db issue

--- db/migrate/20170222173909_add_quantity_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddQuantityToProducts < ActiveRecord::Migration[5.0]
-  def change
-    add_column :products, :prod_quantity, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301202702) do
+ActiveRecord::Schema.define(version: 20170301202140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20170301202702) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "trans_type"
+    t.string   "type"
   end
 
   create_table "products", force: :cascade do |t|
